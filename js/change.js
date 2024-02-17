fa14a0a10414d6ae442a5b43cb7e5c732ac6e577
stotoki <totoki.sayoko@gmail.com>
2024-02-17 11:44:20 +0900

    setting let url

--- js/change.js
+++ js/change.js
@@ -1,56 +1,41 @@
-// let url = "https://www.googleapis.com/books/v1/volumes?q="
+let url = "https://www.googleapis.com/books/v1/volumes?q="
 
-// url += $("#searchButton").on("click", function(){
-//     let element = document.getElementById("keyword");
-//     console.log(element.value);
-//     return element.value;
-// })
-// console.log("here");
-// console.log(searchWord);
-
-$("#searchButton").on("click", function(){
-    let element = document.getElementById("keyword");
-    console.log(element.value);
-    return element.value;
-})
-
-// ひとまず、q＝レシピだが、q=検索フォームに記載されたワードにしたい。
-$.getJSON("https://www.googleapis.com/books/v1/volumes?q=レシピ",
+function get(str){$.getJSON(str,
     function (data) {
-        // console.dir(data);
         let view = "";
         view += '<ul>';
         for (let i = 0; i < 3; i++) {
             let item = data.items[i];
             view += '<li>題名：' + item.volumeInfo.title + '</li>';
-            // view += '<li id=buttonId class="rearrangeButton">ボタン</li>';
         }
         view += '</ul>';
         $("#content").html(view);
     }
-);
+)};
+
+$("#searchButton").on("click", function(){
+    let element = document.getElementById("keyword");
+    url += element.value;
+    // console.log(url);
+    get(url);
+});
 
 $("#edit1").on("click", function(){
     if (localStorage.getItem("memo_1")) {
-        //↓localStorageはobjectなのに、何故かlocalStorage.lengthで長さをとれてしまった。。。
-        // console.log(`memo_${localStorage.length}`);
-        // console.log(localStorage);
-        // console.log(Object.keys(localStorage).length);
         let key = `memo_${Object.keys(localStorage).length}`;
         const value = localStorage.getItem(key);
         $("#text_area").val(value);
     }
     else {
-        $.getJSON("https://www.googleapis.com/books/v1/volumes?q=レシピ",
-            function (data) {
-                let res = "";
-                res += "題名："
-                res += data.items[0].volumeInfo.title + "         ";
-                // res += '<br>'
-                res += "概要："
-                res += data.items[0].volumeInfo.description
-                $("#text_area").val(res);
-            });
+        $.getJSON(url, function (data) {
+            let res = "";
+            res += "題名："
+            res += data.items[0].volumeInfo.title + "         ";
+            // res += '<br>'
+            res += "概要："
+            res += data.items[0].volumeInfo.description
+            $("#text_area").val(res);
+        });
     }
 });
 
@@ -61,15 +46,14 @@ $("#edit2").on("click", function(){
         $("#text_area").val(value);
     }
     else {
-        $.getJSON("https://www.googleapis.com/books/v1/volumes?q=レシピ",
-            function (data) {
-                let res = "";
-                res += "題名："
-                res += data.items[1].volumeInfo.title + "         ";
-                res += "概要："
-                res += data.items[1].volumeInfo.description
-                $("#text_area").val(res);
-            });
+        $.getJSON(url, function (data) {
+            let res = "";
+            res += "題名："
+            res += data.items[1].volumeInfo.title + "         ";
+            res += "概要："
+            res += data.items[1].volumeInfo.description
+            $("#text_area").val(res);
+        });
     }
 });
 
@@ -80,15 +64,14 @@ $("#edit3").on("click", function(){
         $("#text_area").val(value);
     }
     else {
-        $.getJSON("https://www.googleapis.com/books/v1/volumes?q=レシピ",
-            function (data) {
-                let res = "";
-                res += "題名："
-                res += data.items[2].volumeInfo.title + "         ";
-                res += "概要："
-                res += data.items[2].volumeInfo.description
-                $("#text_area").val(res);
-            });
+        $.getJSON(url, function (data) {
+            let res = "";
+            res += "題名："
+            res += data.items[2].volumeInfo.title + "         ";
+            res += "概要："
+            res += data.items[2].volumeInfo.description
+            $("#text_area").val(res);
+        });
     }
 });
 
